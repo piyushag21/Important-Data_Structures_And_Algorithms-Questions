13419eb899acbce7e3742c8ed5463a3a6c4c2c3b
piyushag21 <piyush.ag.21@gmail.com>
2021-01-29 23:08:38 +0530

    Complete

--- Greedy/Activity-Selection.cpp
+++ Greedy/Activity-Selection.cpp
@@ -1,7 +1,46 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int maxMeetings(int *, int *, int);
+
+int main() 
 {
-    cout << "Hello" << "\n";
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        int ans = maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+
+int maxMeetings(int start[], int end[], int n) 
+{
+    int count = 0;
+    multimap<int , int>time;
+    for(int itr = 0; itr < n; itr++)
+    {
+        time.insert(pair <int, int> (end[itr], start[itr]));
+    }
+    auto x = time.begin();
+    count++;
+    for(auto itr = std::next(time.begin()); itr != time.end(); itr++)
+    {
+        if(x->first < itr->second)
+        {
+            count++;
+            x = itr;
+        }
+    }
+   
+    return count;
 }
